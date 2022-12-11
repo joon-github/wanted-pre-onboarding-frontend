88fc802d2c869e569e47a87eada83ddbb360b6e5
beomJoon <ehdfkd2000@naver.com>
2022-12-11 13:10:42 +0900

    Home 작성

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,7 +1,25 @@
-import React from "react";
+import styled from "styled-components";
 
 const Home = () => {
-  return <div>TodoList</div>;
+  return (
+    <HomeStyle>
+      <p className="content">
+        TodoList <br /> 회원 가입 후 이용해주세요
+      </p>
+    </HomeStyle>
+  );
 };
-
+const HomeStyle = styled.main`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 50px;
+  height: calc(100vh - 50px);
+  background-color: rgb(244, 244, 244);
+  .content {
+    color: gray;
+    font-size: 30px;
+    font-weight: bold;
+  }
+`;
 export default Home;
